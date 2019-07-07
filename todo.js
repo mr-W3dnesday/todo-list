093a7f9d1fe371db6938304df15fff103c0bd8cb
Rhys Coghlan <rhyscog98@gmail.com>
2019-07-07 18:04:12 +0100

    create and add to list. mark todos as done

--- todo.js
+++ todo.js
@@ -1,12 +1,39 @@
-function newTodo(task){
-    todolist.append('<li>' + task + '</li>');
+function enterTodo(){
+    var task = inputBox.val();
+    inputBox.val('');
+    if (task){
+        addItem(task);
+    }
+    // change motivational message inside input box when todo is uploaded
+    // popup confirming todo has been added
 }
 
-var newTodo = 'Buy hummus';
+function addItem(task){
+    var item = $("<li>").text(task);
+    todolist.append(item);
+    item.click(function(){
+        $(this).toggleClass('done');
+    })
+}
+
+function compileList(){
+    savedItems.forEach(addItem);
+}
+
 var todolist = $('#todolist');
+
+// style each todo card
+
+// create todo list using JS. Add cookie to store user todos
+var savedItems = ['Do homework', 'Pick up litter', 'Become all knowing', 'Teach monkey to shoplift'];
+compileList();
+
+// new todo items
 var inputBox = $('input');
 var addButton = $('.addbtn');
+addButton.click(enterTodo);
+inputBox.bind('change',enterTodo);
 
-addButton.bind('click',function(){
-    console.log('testing');
-})
+// behavior when hovering over list items
+
+// allow users to delete todos from list
