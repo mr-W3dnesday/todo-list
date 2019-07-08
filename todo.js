713f9b646919324eb8abc44dd66839390c131310
Rhys Coghlan <rhyscog98@gmail.com>
2019-07-08 19:35:59 +0100

    alter appearance

--- todo.js
+++ todo.js
@@ -1,13 +1,14 @@
+// style each todo card
 function generateCard(task){
     var cardA = `
-        <div class="card mt-2 py-2"
+        <div class="card border-light text-white bg-info mt-2 py-2"
             <div class="card-body">
                 <h5 class="card-text">`;
     var cardB = `</h5>
-                <h6 class="card-date mb-2 text-muted">`;
+                <h6 class="card-date mb-2 text-white">`;
     var cardC = `</h6>
                 <div class="card-btn">
-                    <button class="btn complete btn-primary" type="button" id="button-addon2"><i class="far fa-check-square"></i></button>
+                    <button class="btn complete btn-light" type="button" id="button-addon2"><i class="far fa-check-square"></i></button>
                     <button class="btn delete btn-danger" type="button" id="button-addon2"><i class="far fa-trash-alt"></i></button>
                 </div>
             </div>
@@ -25,7 +26,6 @@ function enterTodo(){
         // change motivational message inside input box when todo is uploaded
         changeMessage();
         // popup confirming todo has been added
-        alert('New item added: ' + task);
     }
 }
 
@@ -41,14 +41,16 @@ function addItem(task){
     var item = $("<div>").html(generateCard(task));
     item.addClass('card-container col-12 col-sm-6 col-md-4');
     todolist.append(item);
-    //selecting whole todo list, not last element!!
+    item.fadeIn();
+    // add done functionality to whole card (including button)
     item.click(function(){
         $(this).toggleClass('done');
     })
     // create event listener for delete button
     var deleteBtn = $('.delete');
     deleteBtn.click(function(){
-        $(this).closest('.card-container').remove(); 
+        $(this).closest('.card-container').fadeOut(function(){
+            $(this).remove()}); 
     })
 }
 
@@ -58,9 +60,7 @@ function compileList(){
 
 var todolist = $('#todolist');
 
-// style each todo card
-
-// create todo list using JS. Add cookie to store user todos
+// create todo list using JS
 var savedItems = ['Do homework', 'Pick up litter', 'Become all knowing', 'Teach monkey to shoplift'];
 var messages = ['Get Organised!','What\'s on your mind?','You busy, busy bee...']
 var inputBox = $('input');
@@ -68,6 +68,13 @@ var addButton = $('.addbtn');
 changeMessage();
 compileList();
 
+// Add cookie to store user todos
+var newSavedItems = []
+var itemText = $('.card-text');
+var itemDate = $('.card-date');
+// need to map these arrays into an object
+var text = itemText.map(function(item){return item.text})
+
 // new todo items
-addButton.click(enterTodo);
-inputBox.bind('change',enterTodo);
+addButton.on('click',enterTodo);
+inputBox.on('change',enterTodo);
